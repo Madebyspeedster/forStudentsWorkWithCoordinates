08c2a46eee1f55fa6602c66a2269db5885415f30
Madebyspeedster <madebyspeedster@gmail.com>
2019-04-21 11:11:01 +0300

    ADDED basic code

---
+++ workWithCoordinates.js
@@ -0,0 +1,18 @@
+const el = document.querySelector("div");
+
+function addLabel(element, text) {
+    const coordinates = el.getBoundingClientRect();
+    console.log(coordinates);
+    const message = document.createElement("p");
+    message.style.cssText = `
+            position: fixed; top: ${coordinates.top - 15}px;
+            left: ${coordinates.left}px;
+            color: orangered;
+            font-weight: bold;
+        `;
+    message.textContent = text;
+    el.onmouseover = () => document.body.appendChild(message);
+    el.onmouseleave = () => document.body.removeChild(message);
+}
+
+addLabel(el, "That is True!)!");
